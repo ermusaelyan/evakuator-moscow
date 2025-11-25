5b340832d77a91705b64b3c54cd1ca34c5676116
unknown <ermusaelian@gmail.com>
2025-11-25 15:08:32 +0400

    test add

---
+++ src/components/EvacuatorCard/EvacuatorCard.tsx
@@ -0,0 +1,15 @@
+import styles from './EvacuatorCard.module.scss'
+
+// interface Props {
+//   name: string
+//   price: string
+//   image: string
+// }
+
+export default function EvacuatorCard() {
+  return (
+    <div className={styles.card}>
+      <h1>Test</h1>
+    </div>
+  )
+}
