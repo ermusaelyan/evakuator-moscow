1e229187ff59a3fc0a535baa162239a056ec82a2
ermusaelyan <ermusaelian@gmail.com>
2025-12-09 16:55:46 +0400

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-import EvacuatorCard from '@/components/EvacuatorCard/EvacuatorCard';
-import services from '@/data/services.json';
+import EvacuatorCard from '@/components/EvacuatorCard/EvacuatorCard'
+import services from '@/data/services.json'
+import s from './page.module.scss'
 
 export default function Home() {
   return (
-      <main>
-        <h1>Услуги эвакуатора в Москве</h1>
-        <div style={{ display: 'flex', gap: '1rem' }}>
-          {services.map((service) => (
-              <EvacuatorCard key={service.id} />
-          ))}
-        </div>
-      </main>
-  );
+    <main className={s.wrapper}>
+      <h1>Услуги эвакуатора в Москве</h1>
+      <div style={{ display: 'flex', gap: '1rem' }}>
+        {services.map((service) => (
+          <EvacuatorCard key={service.id} />
+        ))}
+      </div>
+    </main>
+  )
 }
